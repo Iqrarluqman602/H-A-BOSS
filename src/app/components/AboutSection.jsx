942efeee9685d19a616a65f94195bda1555ff6da
iqrar luqman <raoiqrar918@gmail.com>
2023-11-25 16:39:31 -0800

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,109 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>HTML 5</li>
+        <li>CSS 3</li>
+        <li>BOOTSTRAP</li>
+        <li>JQUERY</li>
+        <li>JavaScript</li>
+        <li>PHP</li>
+        <li>PYTHON</li>
+        <li>WORDPRESS</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+       
+        <li>Bachelors in Computer Science from ILMA University.</li>
+        
+        <li>Intermediate in Pre-Engineering from Ziauddin Board.</li>
+       
+        <li>Matriculation in science from Board of Secondary Education Karachi.</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+       
+        <li> Teacher Training Certificate in participating in Karachi English Teacher Training.</li>
+      <li>Certificate in Professional Diploma in Information Technology</li>
+      <li>Certificate in participating in Web Designing & Freelancing workshop.</li>
+      <li>Certificate in participating in ILMA Techno Quest Fall 2021.</li>
+      <li>Certificate in Diploma In Artificial Intelligence.</li>
+      <li>Certificate in AI For Everyone.</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a web developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, Php , Bootstrap , HTML, CSS, and Wordpress. I am a quick learner and I am always
+            looking to expand my knowledge and skill set. I am a team player and
+            I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+       <h2><span>Hooria Aziz</span></h2> 
+        <p className="text-slate-600">Iqrar luqman all rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
